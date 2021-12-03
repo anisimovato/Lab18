2b9f9aba9a968c78f5ef2c18c247091932302060
Татьяна Анисимова <kulmina@mail.ru>
2021-12-03 10:08:40 +0300

    333

--- Lab18/Program.cs
+++ Lab18/Program.cs
@@ -12,66 +12,60 @@ namespace Lab18
         static void Main(string[] args)
         {
             string str = "{([}])";
-
-            
-            Stack <char>stack1 = new Stack<char>();
-            foreach ( char skb in str)
-
-
-                switch (skb)
-                {
-                    case '{':
-                        stack1.Push('}');
-                        break;
-                    case '(':
-                        stack1.Push(')');
-                        break;
-                    case '[':
-                        stack1.Push(']');
-                        break;
-                    case '}':
-                        char i = stack1.Peek();
-                        if (i == skb)
-                        {
-                            stack1.Pop();
-                        }
-                        
-                        break;
-                    case ')':
-                        char j = stack1.Peek();
-                        if (j == skb)
-                        {
-                            stack1.Pop();
-                        }
-                        
-                        break;
-                    case ']':
-                        char n = stack1.Peek();
-                        if (n == skb)
-                        {
-                            stack1.Pop();
-                        }
-                        
-                        break;
-                }
             try
             {
-                stack1.Pop();
-                Console.WriteLine("Скобки расставлены не корректно");
-            }
-            catch (InvalidOperationException)
-            {
-                Console.WriteLine("Стек пуст.скобки расставлены корректно");
-            }
-            //char k = stack1.Peek();
-            //if (k == 0)
-            //    Console.WriteLine("Скобки расставлены корректно");
+
+                Stack<char> stack1 = new Stack<char>();
+                foreach (char skb in str)
+                {
 
 
+                    switch (skb)
+                    {
+                        case '{':
+                            stack1.Push('}');
+                            break;
+                        case '(':
+                            stack1.Push(')');
+                            break;
+                        case '[':
+                            stack1.Push(']');
+                            break;
+
+                        default:
+                            if (stack1.Peek() == skb)
+                            {
+                                stack1.Pop();
+                            }
+                            else
+                            {
+                                Exception ex = new Exception("Скобки расставлены НЕ корректно");
+                                throw ex;
+
+                            }
+                            break;
+                    }
+                }
+                if (stack1.Count() == 0)
+                {
+                    Console.WriteLine("Скобки расставлены корректно");
+                }
+                else
+                {
+                    Console.WriteLine("Скобки расставлены не корректно");
+                }
                 Console.ReadKey();
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadKey();
+            }
         }
     }
 }
+
 
 
 
@@ -80,3 +74,34 @@ namespace Lab18
 //    Указание: задача решается однократным проходом по символам заданной строки слева направо; для каждой открывающей скобки в с
 //    троке в стек помещается соответствующая закрывающая, каждая закрывающая скобка в строке должна соответствовать скобке из вершины стека 
 //    (при этом скобка с вершины стека снимается); в конце прохода стек должен быть пуст.
+
+
+//case '}':
+//    char i = stack1.Peek();
+//    if (i == skb)
+//    {
+//        stack1.Pop();
+//    }
+
+
+
+//    break;
+//case ')':
+//    char j = stack1.Peek();
+//    if (j == skb)
+//    {
+//        stack1.Pop();
+//    }
+
+//    break;
+//case ']':
+//    char n = stack1.Peek();
+//    if (n == skb)
+//    {
+//        stack1.Pop();
+//    }
+
+//    break;
+
+
+
